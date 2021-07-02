6dd42a7f791ebd41aff417c8107cc45e626410b0
DasVelika <86857220+DasVelika@users.noreply.github.com>
2021-07-02 17:09:36 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+package ec.edu.espol.proyecto;
+
+import ec.edu.espol.model.Vendedor;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Vendedor v1;
+        
+        
+        
+    }
+    
+}
